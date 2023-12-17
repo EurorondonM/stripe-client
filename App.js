8267c85c52703f0de942040d49acd104b2705b4b
eurorondon03 <eurorondon03@gmail.com>
2023-12-17 09:25:15 -0400

    first test stripe done

--- App.js
+++ App.js
@@ -1,20 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import StripeApp from "./src/StripeApp";
+import { StripeProvider } from "@stripe/stripe-react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <StripeProvider publishableKey="pk_test_51ODTD4AWKgvi1dxbq0OKvsNQ8sy3BNYaDpGVp5iG989uCGAkHhs5UPf5B6SLfmNONAwBvr6qZOEPnXGUIjskbbbC002UTbBwXt">
+      <StripeApp />
+    </StripeProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
